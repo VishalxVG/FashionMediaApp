ab260df21aa70f87d0ba417322e533bbdf3af6ab
Vishal Ghosh <avatarvishal1551@gmail.com>
2024-02-03 01:03:41 +0530

    Provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:fashion_app/components/themes/colors.dart';
+import 'package:fashion_app/components/utils/provider/UserProvider.dart';
 import 'package:fashion_app/firebase_options.dart';
 
 import 'package:fashion_app/services/auth/auth_gate.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,11 +20,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fashion Social Media App',
-      theme: lightMode,
-      debugShowCheckedModeBanner: false,
-      home: const AuthGate(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UserProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Fashion Social Media App',
+        theme: lightMode,
+        debugShowCheckedModeBanner: false,
+        home: const AuthGate(),
+      ),
     );
   }
 }
